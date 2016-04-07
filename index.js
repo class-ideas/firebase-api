4810a5a25bea30792d48026a791f88ed6498f96f
JD Isaacks <jd@jisaacks.com>
2016-04-06 21:52:08 -0400

    Remove file

--- index.js
+++
@@ -1,6 +0,0 @@
-var firebaseAPI = require('./dist').default;
-
-var users = firebaseAPI('contactz').resource('users');
-
-users.put({name: 'Humanoid', email: 'humane@somethingelse.com', id: '-KEhLJbeFT7yC4WJLNbK'})
-  .then(console.log.bind(console)).catch(console.log.bind(console));
